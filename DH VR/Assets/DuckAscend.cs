65947a99517691feb3bcabb1ecb04f62ef8ab16d
Paweł Dziedzic <blavikenn@gmail.com>
2018-11-25 17:17:52 +0100

    Created prefabs for models, reworked main scene, checked "Generate lightmap UV for some objects"

---
+++ DH VR/Assets/DuckAscend.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckAscend : MonoBehaviour {
+    public float m_Speed;
+
+    // Update is called once per frame
+    void Update() {
+        transform.position += Vector3.up * Time.deltaTime * m_Speed;
+    }
+}
