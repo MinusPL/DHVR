a99545470b249f2073e2f1af6755e579c176fe8d
Paweł Dziedzic <blavikenn@gmail.com>
2018-11-27 12:47:07 +0100

    Simple Duck Controller script and spawner

---
+++ DH VR/Assets/_Scripts/DuckController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckController : MonoBehaviour {
+    public float m_NextWaipointTreshold;
+    public float m_Speed;
+
+    public static event System.Action<DuckController> OnDuckDeath;
+
+    private List<Vector3> m_Waipoints;
+    private int m_CurrentWaitpointIndex;
+
+    public void Initialize(List<Vector3> waipoints) {
+        m_Waipoints = waipoints;
+    }
+
+    private void Update() {
+        var target = m_Waipoints[m_CurrentWaitpointIndex];
+        var dir = (target - transform.position).normalized;
+
+        transform.position += dir * m_Speed * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(target);
+        
+        if (Vector3.SqrMagnitude(transform.position - target) < m_NextWaipointTreshold * m_NextWaipointTreshold) {
+            m_CurrentWaitpointIndex++;
+            if (m_CurrentWaitpointIndex >= m_Waipoints.Count) {
+                Flee();
+            }
+        }
+    }
+
+    private void Flee() {
+        Destroy(gameObject);
+    }
+
+    public void Death() {
+        OnDuckDeath?.Invoke(this);
+    }
+
+    private void OnDrawGizmos() {
+        if (m_Waipoints != null)
+            for (int i = 0; i < m_Waipoints.Count - 1; i++) {
+                Gizmos.DrawLine(m_Waipoints[i], m_Waipoints[i + 1]);
+            }
+    }
+}
---
+++ DH VR/Assets/_Scripts/DuckSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckSpawner : MonoBehaviour {
+    public Vector3 m_MovementBoxSize;
+    public DuckController m_DuckPrefab;
+
+    private void Start() {
+        Spawn();
+        Spawn();
+    }
+
+    void Spawn() {
+        var waypoints = new List<Vector3>();
+        for (int i = 0; i < 5; i++) {
+            float x = Random.Range(-m_MovementBoxSize.x, m_MovementBoxSize.x);
+            float y = Random.Range(-m_MovementBoxSize.y, m_MovementBoxSize.y);
+            float z = Random.Range(-m_MovementBoxSize.z, m_MovementBoxSize.z);
+
+            var point = new Vector3(x, y, z) / 2f + transform.position;
+            waypoints.Add(point);
+        }
+
+        float spawnX = Random.Range(-m_MovementBoxSize.x, m_MovementBoxSize.x);
+        float spawnZ = Random.Range(-m_MovementBoxSize.z, m_MovementBoxSize.z);
+        var spawnPoint = new Vector3(spawnX, -m_MovementBoxSize.y, spawnZ)/2f + transform.position;
+        var duck = Instantiate(m_DuckPrefab, spawnPoint, Quaternion.identity);
+        duck.Initialize(waypoints);
+    }
+
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, m_MovementBoxSize);
+    }
+}
